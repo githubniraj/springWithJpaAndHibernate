84379f37668d8996ccabfab90b20e14f73093f8b
niraj <niraj.drn@gmail.com>
2017-04-18 19:07:27 -0500

    added spring data jpa in the repository layer

--- src/main/java/com/pluralsight/repository/ExerciseRepository.java
+++ src/main/java/com/pluralsight/repository/ExerciseRepository.java
@@ -1,9 +1,11 @@
 package com.pluralsight.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Exercise;
 
-public interface ExerciseRepository {
-	
-	Exercise save (Exercise exercise);
+@Repository("exerciseRepository")
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
 }
